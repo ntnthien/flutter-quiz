8f3dcc793d0c31d6f8152b617afb83445f65239e
Do Nguyen <daniel@zincer.com>
2018-03-31 18:28:51 +0700

    initial commit

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Material(
+      color: Colors.green,
+      child: new InkWell(
+        onTap: () => print("tapped"),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              "Lets Quizz",
+              style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            new Text(
+              "Tap to start!",
+              style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
