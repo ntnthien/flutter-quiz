296704b425d3bb06a6d61db669e2b7b9ea85d036
Do Nguyen <daniel@zincer.com>
2018-04-02 17:22:51 +0700

    Add ResultOverlay + ScorePage

---
+++ lib/UI/result_overlay.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class ResultOverlay extends StatefulWidget {
+  final bool _isCorrect;
+  final VoidCallback _onTap;
+
+  ResultOverlay(this._isCorrect, this._onTap);
+
+  @override
+  State<StatefulWidget> createState() => new ResultOverlayState();
+}
+
+class ResultOverlayState extends State<ResultOverlay>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _iconAnimation;
+
+  AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        duration: new Duration(seconds: 2), vsync: this);
+    _iconAnimation = new CurvedAnimation(
+        parent: _iconAnimationController, curve: Curves.elasticOut);
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
+
+  @override
+    void dispose() {
+      _iconAnimationController.dispose()
+      super.dispose();
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.black54,
+      child: new InkWell(
+        onTap: () => widget._onTap(),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              decoration: new BoxDecoration(
+                  color: Colors.white, shape: BoxShape.circle),
+              child: new Transform.rotate(
+                angle: _iconAnimation.value * 2 * PI,
+                child: new Icon(
+                  widget._isCorrect ? Icons.done : Icons.clear,
+                  size: 80.0 * _iconAnimation.value,
+                ),
+              ),
+            ),
+            new Padding(padding: new EdgeInsets.only(bottom: 20.0)),
+            new Text(
+              widget._isCorrect ? "Correct" : "Wrong",
+              style: new TextStyle(color: Colors.white, fontSize: 30.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -4,6 +4,8 @@ import '../utils/question.dart';
 import '../utils/quiz.dart';
 import '../UI/answer_button.dart';
 import '../UI/question_text.dart';
+import '../UI/result_overlay.dart';
+import './score_page.dart';
 
 class QuizPage extends StatefulWidget {
   @override
@@ -11,19 +13,60 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage> {
+  Question _currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Elon Musk is human", false),
+    new Question("You are handsome", true)
+  ]);
+  String questionText;
+  int questionNumber;
+  bool _isCorrect;
+  bool _overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _currentQuestion = quiz.nextQuestion;
+    questionText = _currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    _isCorrect = (_currentQuestion.answer == answer);
+    quiz.answer(_isCorrect);
+    this.setState(() {
+      _overlayShouldBeVisible = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Stack(
-      children: <Widget>[
-        new Column(
-          children: <Widget>[
-            new AnswerButton(true, () => print("answered true")),
-            new QuestionText("Are you fine???", 1),
-            new AnswerButton(false, () => print("answered false"))
-          ], // This is main page
-        )
-      ],
-    );
+    return new Stack(fit: StackFit.expand, children: <Widget>[
+      new Column(
+        children: <Widget>[
+          new AnswerButton(true, () => handleAnswer(true)),
+          new QuestionText(questionText, questionNumber),
+          new AnswerButton(false, () => handleAnswer(false)),
+        ], // This is main page
+      ),
+      _overlayShouldBeVisible
+          ? new ResultOverlay(_isCorrect, () {
+              if (quiz.length == questionNumber) {
+                Navigator.of(context).pushAndRemoveUntil(
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            new ScorePage(quiz.score, quiz.length)),
+                    (Route route) => route == null);
+              }
+              _currentQuestion = quiz.nextQuestion;
+              this.setState(() {
+                _overlayShouldBeVisible = false;
+                questionText = _currentQuestion.question;
+                questionNumber = quiz.questionNumber;
+              });
+            })
+          : new Container()
+    ]);
   }
 }
